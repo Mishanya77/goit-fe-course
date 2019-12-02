80cb5548da02acb7f30e5d1ecfedd74d651a5b41
Misha <gorban77@gmail.com>
2019-12-02 11:05:26 +0200

    hw2
    ver2

--- JS/hw-2/task5.js
+++ JS/hw-2/task5.js
@@ -1,4 +1,19 @@
-"use strict"
+"use strict";
 
-const string = 'Latest technology news';
-console.log(string.includes('spam,sale'))
+const checkForSpam = string => {
+  let phrase = string.toLowerCase();
+
+  if (phrase.indexOf("sale") !== -1 || phrase.indexOf("spam") !== -1) {
+    return true;
+  }
+
+  return false;
+};
+
+console.log(checkForSpam("Latest technology news")); // false
+
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+
+console.log(checkForSpam("Get best sale offers now!")); // true
+
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
